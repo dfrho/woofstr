82f6894b6be264e8f35502f2e07c419da53f3fc9
David Rhodes <david@espressocode.tech>
2021-10-03 22:45:00 -0500

    Revert "trying something"
    This reverts commit 29d53fa27e1cdd7d63ac01abb2fd44c8fecb90fe.

--- src/components/ChatMessages.js
+++ src/components/ChatMessages.js
@@ -11,7 +11,6 @@ export default function ChatMessages({
   const previousMessages = usePrevious(messages);
   if (previousMessages?.length < messages?.length) {
     const sound = new Audio('../../popup-sound.wav');
-    sound.volume = 1;
     sound.play();
   }
   if (messages) {
