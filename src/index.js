918350556719cb3fbe9312b5c54ff4a6e60b19c5
David Rhodes <david@espressocode.tech>
2022-01-04 06:44:00 -0600

    reinstalls fullstory

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
+import * as FullStory from '@fullstory/browser';
+
+FullStory.init({ orgId: '17KZ2T' });
 
 ReactDOM.render(
   <React.StrictMode>
